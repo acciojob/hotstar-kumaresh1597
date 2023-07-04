44f64ab62125d3b83030042a9a6218e4e4cbf2fe
kumaresan <kumaresh.1597@gmail.com>
2023-07-04 11:54:51 +0530

    production house added

--- src/main/java/com/driver/services/ProductionHouseService.java
+++ src/main/java/com/driver/services/ProductionHouseService.java
@@ -14,8 +14,11 @@ public class ProductionHouseService {
     ProductionHouseRepository productionHouseRepository;
 
     public Integer addProductionHouseToDb(ProductionHouseEntryDto productionHouseEntryDto){
+        ProductionHouse productionHouse = new ProductionHouse(productionHouseEntryDto.getName());
+        productionHouse.setRatings(0);
 
-        return  null;
+        productionHouse =  productionHouseRepository.save(productionHouse);
+        return  productionHouse.getId();
     }
 
 
